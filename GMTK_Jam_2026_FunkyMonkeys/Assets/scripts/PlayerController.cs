5bb8805f97fb1098d046eca47ad2bbfe089a0652
christophershin <dinojun323@gmail.com>
2026-07-23 17:24:26 -0700

    player ragdolll

--- GMTK_Jam_2026_FunkyMonkeys/Assets/scripts/PlayerController.cs
+++ GMTK_Jam_2026_FunkyMonkeys/Assets/scripts/PlayerController.cs
@@ -8,7 +8,6 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-
     private float horizontal;
     private bool FireDMG;
     private bool inflictBurn;
@@ -32,18 +31,99 @@ public class PlayerController : MonoBehaviour
     public TMP_Text score;
 
 
-    [SerializeField] private Rigidbody2D rb;
+    private Rigidbody2D rb;
     [SerializeField] private Transform GroundCheck;
     [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private GameObject parent;
+
+    private bool CanStopTime;
+
+    public GameObject deathMessage;
+    public GameObject winMessage;
+
+    public Animator animator;
+
+    [SerializeField] private AudioClip[] PlayerSounds;
+
     void Start()
     {
-        
+        PlayerHP = MaxPlayerHP;
+        //deathMessage.SetActive(false);
+        //winMessage.SetActive(false);
+        Time.timeScale = 1.0f;
+        //PlayerHealthBar.value = MaxPlayerHP;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
+
+
+
     void Update()
     {
+
+
         horizontal = Input.GetAxisRaw("Horizontal");
+
+        //if (horizontal != 0f)
+        //{
+        //    animator.SetBool("IsWalking", true);
+
+        //}
+
+        //if (horizontal == 0f)
+        //{
+        //    animator.SetBool("IsWalking", false);
+
+        //}
+
+
+        // jumping 
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+
+            //float cliplength = PlayerSounds[0].length;
+            //SoundManager.SoundInst.PlaySoundFXClip(PlayerSounds[0], transform, 1f, cliplength, 0.6f, 0.7f, 15);
+
+            gravityMultiplier = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+            doubleJumpCooldown = true;
+        }
+
+        // double jump
+        if (Input.GetKeyDown(KeyCode.Space) && !IsGrounded() && doubleJumpCooldown == true)
+        {
+            //float cliplength = PlayerSounds[0].length;
+            //SoundManager.SoundInst.PlaySoundFXClip(PlayerSounds[0], transform, 1f, cliplength, 0.6f, 0.7f, 15);
+
+            doubleJumpCooldown = false;
+            gravityMultiplier = 0f;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+
+        }
+
+
+
+        //gravityMulitplication();
+        fakeGravity();
+
+
+        //Display Player Health
+        //PlayerHealthBar.value = PlayerHP;
+
+        if (PlayerHP <= 0)
+        {
+            PlayerHP = 0;
+            Debug.Log("GAME OVER");
+            deathMessage.SetActive(true);
+            Time.timeScale = 0f;
+        }
+
+
+        //Flip();
+
+
     }
 
     private void FixedUpdate()
@@ -52,4 +132,89 @@ public class PlayerController : MonoBehaviour
 
 
     }
+
+
+
+    public bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(GroundCheck.position, 0.2f, groundLayer);
+    }
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+
+
+
+    void fakeGravity()
+    {
+        if (rb.linearVelocity.y > MaxVelocityY * -1)
+        {
+            if (rb.linearVelocity.y < minFallingActivation && !IsGrounded())
+            {
+
+                gravityMultiplier += Time.deltaTime;
+
+                if (gravityMultiplier > maxGravityMulti)
+                {
+                    rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 1.5f);
+
+                    gravityMultiplier = 0f;
+                }
+
+            }
+            else
+            {
+                gravityMultiplier = 0f;
+            }
+        }
+    }
+
+
+
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "DeathBox")
+        {
+            PlayerHP = 0;
+        }
+
+        if (other.gameObject.layer == 3 && IsGrounded())
+        {
+            //SoundManager.SoundInst.PlaySoundFXClip(PlayerSounds[2], transform, 0.6f, 0.4f, 0.6f, 0.6f, 20);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.gameObject.tag == "Projectile")
+        {
+
+            //TakePlayerDamage(collision.gameObject.GetComponent<Projectiles>().damage);
+            //collision.gameObject.GetComponent<Projectiles>().damage = 0;
+        }
+
+        if (collision.gameObject.layer == 3)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, 0f);
+        }
+
+        if (collision.gameObject.tag == "LevelEnd")
+        {
+            winMessage.SetActive(true);
+        }
+    }
+
+
+
 }
